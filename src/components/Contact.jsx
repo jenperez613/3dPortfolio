75832eef174356cfbf4caae9c672070e3757265c
Jen Perez <jenperez613@icloud.com>
2023-06-03 17:28:14 -0400

    fixed email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
     setLoading(true);
     emailjs
       .send(
-        service_af2qzxq,
-        template_plf2deh,
+        'service_af2qzxq',
+        'template_plf2deh',
         {
           from_name: form.name,
           to_name: 'Jen Perez',
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: 'jen@jenperez.dev',
           message: form.message,
         },
-        FzVn7y_EW66tXO-jT
+        'FzVn7y_EW66tXO-jT'
       )
       .then(
         () => {
